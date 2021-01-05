a7318c3f85acd1ba0963c5eea65ceb4909b16041
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-01-05 17:12:19 +0100

    Render html from server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 // Connect DB
-connectDB(); 
+connectDB();
+
+// Render html-pages
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Init middleware
 app.use(express.json({ extended: false }));
@@ -14,6 +18,7 @@ app.get('/', (req,res) => res.send('API Running'));
 // Define router
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
+
 
 const PORT = process.env.PORT || 5000;
 
