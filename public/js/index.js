54acda93ca783f2ba1ed89e5bc2961d5f6b87f2a
OKozmovskaia <olga.kozmovskaia@gmail.com>
2021-01-06 12:52:59 +0100

    Auth form from html connect with database

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+// Register new user
 const formDataUser = document.querySelector('#formRegister');
 
 formDataUser.addEventListener('submit', function(event) {
@@ -12,6 +13,30 @@ formDataUser.addEventListener('submit', function(event) {
   console.log(dataBody);
 
   fetch('/api/user', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: dataBody
+});
+});
+
+// Authentication 
+const formDataLogin = document.querySelector('#formLogin');
+
+formDataLogin.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const formEvent = event.target;
+  // get values from formData
+  let formData = new FormData(formEvent);
+  const plainFormData = Object.fromEntries(formData.entries());
+  console.log(plainFormData);
+  const dataBody = JSON.stringify(plainFormData);
+
+  console.log(dataBody);
+
+  fetch('/api/auth', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
